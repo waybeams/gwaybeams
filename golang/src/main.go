8443f5c38015bbf0af3d58466365eb0f7acf5a76
Luke Bayes <lukebayes@gmail.com>
2018-03-15 16:18:30 -0400

    Backing off to work with GLFW

---
+++ golang/src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"runtime"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(800, 600, "Finding You", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
