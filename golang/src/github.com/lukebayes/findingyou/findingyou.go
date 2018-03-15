521d23b562c0935f578f02b7702c16a0a0dcbaaf
Luke Bayes <lukebayes@gmail.com>
2018-03-15 15:55:50 -0400

    Removed old experimental files

--- golang/src/github.com/lukebayes/findingyou/findingyou.go
+++ golang/src/github.com/lukebayes/findingyou/findingyou.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/lukebayes/gnomplate"
 )
 
 func main() {
-	fmt.Println("Finding You")
-	options := &gnomplate.WindowOptions{
-		Title:  "Finding You",
-		Height: 600,
-		Width:  800,
-	}
-	instance := gnomplate.CreateWindow(options)
-	fmt.Println("Created Application")
-	instance.Open()
+	fmt.Println("Hello World")
+	/*
+		fmt.Println("Finding You")
+		options := &gnomplate.WindowOptions{
+			Title:  "Finding You",
+			Height: 600,
+			Width:  800,
+		}
+		instance := gnomplate.CreateWindow(options)
+		fmt.Println("Created Application")
+		instance.Open()
+	*/
 }
