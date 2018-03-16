3cb5f46aae20f3ab10e0882d8173c8f435b27135
Luke Bayes <lukebayes@gmail.com>
2018-03-16 17:45:19 -0400

    Trying with cairo, since nothing else seems to work

---
+++ golang/src/cairo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	cairo "github.com/ungerik/go-cairo"
+	"time"
+)
+
+func main() {
+	fmt.Println("Cairo Version:", cairo.Version())
+	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, 800, 600)
+	surface.SelectFontFace("serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+	surface.SetFontSize(32)
+	surface.MoveTo(10.0, 50.0)
+	surface.ShowText("Hello World")
+
+	for {
+		fmt.Println("Enter Frame")
+		surface.Paint()
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
