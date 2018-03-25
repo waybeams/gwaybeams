0c266a844601a3f86bb784523efafba6db4d00c5
Luke Bayes <lukebayes@gmail.com>
2018-03-25 12:53:54 -0400

    Added custom assert.TEqual to clean up float bullshit

--- src/assert/assert.go
+++ src/assert/assert.go
@@ -3,8 +3,83 @@ package assert
 import (
 	"errors"
 	"fmt"
+	"math"
+	"reflect"
 	"regexp"
+	"testing"
 )
+
+func messageToString(message ...string) (string, error) {
+	switch len(message) {
+	case 0:
+		return "", nil
+	case 1:
+		return message[0], nil
+	default:
+		return "", errors.New("Custom assertion provided with unexpected messages")
+
+	}
+}
+
+func TStrictEqual(t testing.TB, found interface{}, expected interface{}, message ...string) {
+	if found != expected {
+		msg, err := messageToString(message...)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Errorf("Custom Equal expected %v to strictly equal %v\n%s", found, expected, msg)
+	}
+
+}
+
+func float64EqualsInt(floatValue float64, intValue int) bool {
+	intPart, div := math.Modf(floatValue)
+	if div == 0.0 && int(intPart) == intValue {
+		return true
+	}
+	return false
+}
+
+func TEqual(t testing.TB, found interface{}, expected interface{}, message ...string) {
+	if found != expected {
+		msg, msgErr := messageToString(message...)
+		if msgErr != nil {
+			t.Error(msgErr)
+			return
+		}
+		typeA := reflect.TypeOf(found).String()
+		typeB := reflect.TypeOf(expected).String()
+		switch typeA {
+		case "float64":
+			if typeB == "int" {
+				if !float64EqualsInt(found.(float64), expected.(int)) {
+					t.Errorf("Custom Equal expected %.2g to equal %v\n%s", found, expected, msg)
+				}
+				return
+			}
+		case "int":
+			if typeB == "float64" {
+				if !float64EqualsInt(expected.(float64), found.(int)) {
+					t.Errorf("Custom Equal expected %.2g to equal %v\n%s", expected, found, msg)
+				}
+				return
+			}
+		}
+
+		if found != expected {
+			t.Errorf("Custom Equal expected %v to equal %v\n%s", found, expected, msg)
+		}
+	}
+
+}
+
+func TMatch(t testing.TB, exprStr string, str string) {
+	matched, _ := regexp.MatchString(exprStr, str)
+	if !matched {
+		t.Errorf("Expected: \"%v\", but received: \"%v\"", exprStr, str)
+	}
+}
 
 func Match(exprStr string, str string) {
 	matched, _ := regexp.MatchString(exprStr, str)
--- src/assert/assert_test.go
+++ src/assert/assert_test.go
@@ -1,8 +1,45 @@
 package assert
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
+
+type CustomT struct {
+	testing.T
+	failureWith string
+}
+
+func (c *CustomT) Errorf(format string, args ...interface{}) {
+	c.failureWith = fmt.Sprintf(format, args...)
+}
+
+func (c *CustomT) Error(msgOrErr ...interface{}) {
+	fmt.Println("ERROR CALLED ITH:", msgOrErr)
+	msg := msgOrErr[0]
+	msgType := reflect.TypeOf(msg).String()
+	fmt.Println("msg:", msg)
+
+	switch msgType {
+	case "string":
+		c.failureWith = msg.(string)
+	case "error":
+		c.failureWith = msg.(error).Error()
+	default:
+		panic("Unexpected call to CustomT.Error")
+	}
+}
+
+func NewCustomT() *CustomT {
+	return &CustomT{}
+}
+
+type assertCase struct {
+	failureExpr string
+	found       interface{}
+	expected    interface{}
+}
 
 func TestSuccessAssertions(t *testing.T) {
 	Nil(nil)
@@ -11,6 +48,47 @@ func TestSuccessAssertions(t *testing.T) {
 	False(false)
 	Equal(0, 0)
 	NotEqual(0, 1)
+
+	t.Run("Equality helper", func(t *testing.T) {
+		t.Run("0.0 == 0.0", func(t *testing.T) {
+			ct := NewCustomT()
+			TEqual(ct, 0.0, 0.0)
+			if ct.failureWith != "" {
+				t.Error(ct.failureWith)
+			}
+		})
+
+		t.Run("0.0 == 0", func(t *testing.T) {
+			ct := NewCustomT()
+			TEqual(ct, 0.0, 0)
+			if ct.failureWith != "" {
+				t.Error(ct.failureWith)
+			}
+		})
+
+		t.Run("0 == 0", func(t *testing.T) {
+			ct := NewCustomT()
+			TEqual(ct, 0, 0)
+			if ct.failureWith != "" {
+				t.Error(ct.failureWith)
+			}
+		})
+
+		t.Run("0 == 0.0", func(t *testing.T) {
+			ct := NewCustomT()
+			TEqual(ct, 0, 0.0)
+			if ct.failureWith != "" {
+				t.Error(ct.failureWith)
+			}
+		})
+
+		t.Run("failure with custom message", func(t *testing.T) {
+			ct := NewCustomT()
+
+			TEqual(ct, 1, 2, "Fake custom message")
+			TMatch(t, "Fake custom message", ct.failureWith)
+		})
+	})
 }
 
 // Stop calling panic in assertions!
