4b3adb00276081a5164d3923997668f9d122894f
Luke Bayes <lukebayes@gmail.com>
2018-04-05 15:44:42 -0400

    Renamed local reference from s for sprite to c for component

--- src/display/trait.go
+++ src/display/trait.go
@@ -1,14 +1,12 @@
 package display
 
-import "errors"
-
 // Trait is a concrete factory function that builds a bag of ComponentOptions
 // and applies them to all Selected Components before applying
 // instance-specified options.
 func Trait(b Builder, selector string, opts ...ComponentOption) error {
 	component := b.Peek()
 	if component == nil {
-		return errors.New("Trait definition must be nested inside of a component")
+		panic("Trait definition must be nested inside of a component")
 	}
 	component.PushTrait(selector, opts...)
 	return nil
