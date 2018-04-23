7d24386c4f22d45e88d0190410932e3dab6a945f
Luke Bayes <lukebayes@gmail.com>
2018-04-23 19:04:39 -0400

    Replaced legacy builder with new updater

--- src/display/builder.go
+++ src/display/builder.go
@@ -27,19 +27,13 @@ type Builder interface {
 }
 
 type BaseBuilder struct {
-	clock       clock.Clock
-	isDestroyed bool
-	root        Displayable
-	stack       Stack
-	lastError   error
-	emitter     Emitter
-}
-
-func (b *BaseBuilder) getStack() Stack {
-	if b.stack == nil {
-		b.stack = NewDisplayStack()
-	}
-	return b.stack
+	childrenTypeMap ChildrenTypeMap
+	clock           clock.Clock
+	emitter         Emitter
+	isDestroyed     bool
+	lastError       error
+	root            Displayable
+	stack           Stack
 }
 
 func (b *BaseBuilder) getEmitter() Emitter {
@@ -49,48 +43,131 @@ func (b *BaseBuilder) getEmitter() Emitter {
 	return b.emitter
 }
 
-func (b *BaseBuilder) Clock() clock.Clock {
-	return b.clock
+func (b *BaseBuilder) getStack() Stack {
+	if b.stack == nil {
+		b.stack = NewDisplayStack()
+	}
+	return b.stack
 }
 
 func (b *BaseBuilder) OnEnterFrame(handler EventHandler) Unsubscriber {
 	return b.getEmitter().On(events.EnterFrame, handler)
 }
 
+func (b *BaseBuilder) Clock() clock.Clock {
+	if b.clock == nil {
+		// Go get the clock from the provided Root component's builder
+		b.clock = clock.New()
+	}
+	return b.clock
+}
+
 func (b *BaseBuilder) LastError() error {
 	return b.lastError
 }
 
-// Listen() is the overall application loop and anything that wants to operate
-// in the environment should listen for these events.
-func (b *BaseBuilder) Listen() {
-	var frameHandler = func() bool {
-		root := b.root
-		if root != nil {
-			b.getEmitter().Emit(NewEvent(events.EnterFrame, root, nil))
-
-			if root.ShouldRecompose() {
-				root.RecomposeChildren()
-			}
-			root.Layout()
-		}
-
-		return b.isDestroyed
-	}
-	clock.OnFrame(frameHandler, DefaultFrameRate, b.Clock())
-}
-
-func (b *BaseBuilder) Destroy() {
-	b.stack = nil
-	b.lastError = nil
-	b.root = nil
-	b.isDestroyed = true
-}
-
-// Current returns the current entry in the Builder stack.
-// This method only works while the component declarations are being processed.
 func (b *BaseBuilder) Peek() Displayable {
 	return b.getStack().Peek()
+}
+
+func (b *BaseBuilder) getExistingChild(d Displayable, parent Displayable) Displayable {
+	updateableChildren := parent.updateableChildren()
+	typeName := d.TypeName()
+	kids := updateableChildren[typeName]
+	var result Displayable
+	if kids != nil {
+		if len(kids) > 1 {
+			result = kids[0]
+			updateableChildren[typeName] = kids[1:]
+		} else {
+			result = kids[0]
+			delete(updateableChildren, typeName)
+		}
+		parent.setUpdateableChildren(updateableChildren)
+	}
+	return result
+}
+
+func (b *BaseBuilder) Update(d Displayable) error {
+	b.Push(d)
+	d.Layout()
+	return b.lastError
+}
+
+func (b *BaseBuilder) Push(d Displayable, options ...ComponentOption) {
+	stack := b.getStack()
+
+	// Get the parent element if one exists
+	parent := stack.Peek()
+
+	if parent != nil {
+		existingChild := b.getExistingChild(d, parent)
+		if existingChild == nil {
+			parent.AddChild(d)
+		} else {
+			d = existingChild
+		}
+
+		// Clear the composer function before triggering for what might be
+		// a second time
+		d.Composer(nil)
+	} else {
+		b.root = d
+		d.SetBuilder(b)
+	}
+
+	// One of these options might be a Children(func()), which will recurse
+	// back into this Push function.
+	for _, option := range options {
+		err := option(d)
+		if err != nil {
+			// If an option error is found, bail with it, for now.
+			b.lastError = err
+			return
+		}
+	}
+
+	// Push the element onto the stack
+	stack.Push(d)
+
+	// Create the children type map for this parent
+	d.setUpdateableChildren(b.createChildrenMapFor(d))
+
+	// Process composition function to build children
+	b.callComposeFunctionFor(d)
+
+	// Remove the children that haven't been used.
+	b.clearChildrenFromTypeMap(d)
+
+	// Pop the element off the stack
+	stack.Pop()
+}
+
+func (b *BaseBuilder) clearChildrenFromTypeMap(d Displayable) {
+	types := d.updateableChildren()
+
+	for _, entries := range types {
+		for _, entry := range entries {
+			entry.Parent().RemoveChild(entry)
+		}
+	}
+
+	// Remove the children type mape for this parent
+	b.childrenTypeMap = nil
+}
+
+func (b *BaseBuilder) createChildrenMapFor(d Displayable) ChildrenTypeMap {
+	types := make(map[string][]Displayable)
+	for _, child := range d.Children() {
+		typeName := child.TypeName()
+		entry := types[typeName]
+		if entry == nil {
+			types[typeName] = []Displayable{child}
+		} else {
+			types[typeName] = append(entry, child)
+		}
+	}
+	return types
 }
 
 func (b *BaseBuilder) callComposeFunctionFor(d Displayable) {
@@ -116,61 +193,37 @@ func (b *BaseBuilder) callComposeFunctionFor(d Displayable) {
 	}
 }
 
-// Update will re-render the provided component's children
-func (b *BaseBuilder) Update(d Displayable) error {
-	// NOTE: Brute force update here. Long term, look into creating the
-	// secondary tree and diffing it against the existing tree, only
-	// applying deltas where necessary.
-	b.Push(d)
-	return b.lastError
+func (b *BaseBuilder) Destroy() {
+	b.stack = nil
+	b.lastError = nil
+	b.root = nil
+	b.isDestroyed = true
 }
 
-// Push accepts a new Displayable to place on the stack and processes the
-// optional component composition function if one was provided.
-func (b *BaseBuilder) Push(d Displayable, options ...ComponentOption) {
-	stack := b.getStack()
+func (b *BaseBuilder) Listen() {
+	var frameHandler = func() bool {
+		root := b.root
+		if root != nil {
+			b.getEmitter().Emit(NewEvent(events.EnterFrame, root, nil))
 
-	// Get the parent element if one exists
-	parent := stack.Peek()
-
-	if parent != nil {
-		parent.AddChild(d)
-	} else {
-		// We're looking at the Root element
-		if b.root == nil {
-			b.root = d
+			if root.ShouldRecompose() {
+				root.RecomposeChildren()
+			}
+			root.Layout()
 		}
-		d.SetBuilder(b)
+
+		return b.isDestroyed
 	}
-
-	// One of these options might be a Children(func()), which will recurse
-	// back into this Push function.
-	for _, option := range options {
-		err := option(d)
-		if err != nil {
-			// If an option error is found, bail with it, for now.
-			b.lastError = err
-			return
-		}
-	}
-
-	// Push the element onto the stack
-	stack.Push(d)
-
-	// Process composition function to build children
-	b.callComposeFunctionFor(d)
-
-	// Pop the element off the stack
-	stack.Pop()
+	clock.OnFrame(frameHandler, DefaultFrameRate, b.Clock())
 }
 
 // NewBuilder returns a clean builder instance.
-func NewBuilder() *Updater {
-	return &Updater{}
+func NewBuilder() *BaseBuilder {
+	return &BaseBuilder{}
 }
 
 // NewBuilderUsing runs with provided clock instead of the real one.
 // Mainly used by tests that need to provide a fake clock.
-func NewBuilderUsing(clock clock.Clock) *Updater {
-	return &Updater{clock: clock}
+func NewBuilderUsing(clock clock.Clock) *BaseBuilder {
+	return &BaseBuilder{clock: clock}
 }
